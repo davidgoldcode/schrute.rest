5b184975d537c7b9ccf43b43173a0e51e8665bd5
David Gold <davidgold@hey.com>
2021-01-15 19:46:22 -0500

    Console message update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./server.js");
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Bears beets battlestar galactica. Listening on ${PORT}`);
+  console.log(`Bears beets battlestar galactica.`);
 });
